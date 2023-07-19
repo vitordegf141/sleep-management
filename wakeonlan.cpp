a2e6664ff0c3f177b8997b9542efeb60da05eacc
vitordegf141 <vitordegf@gmail.com>
2023-07-18 23:37:25 -0300

    descobrindo via broadcast

--- wakeonlan.cpp
+++ wakeonlan.cpp
@@ -12,7 +12,8 @@
 
 using namespace std;
 
-#define PORT 12345
+#define PORT 4000
+#define PORT_CLIENT 4021
 #define BUFFER_SIZE 1024
 #define MONITORING_PORT 8889
 
@@ -39,46 +40,79 @@ pthread_mutex_t tableMutex = PTHREAD_MUTEX_INITIALIZER;
 // Executado pelo manager
 // Identifica quais computadores passaram a executar o programa, recebendo e respondendo pacotes em broadcast
 class discovery_subservice{
-public:
 
-    int createSocket(int port) {
+    
+};
+
+int createSocket(int port, char serverName[]) {
         int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+        int yes = 1;
+        int ret;
+        struct hostent *server;
         if (sockfd < 0) {
             std::cerr << "Failed to create socket." << std::endl;
             exit(EXIT_FAILURE);
         }
 
+        ret = setsockopt(sockfd,SOL_SOCKET,SO_BROADCAST,&yes,sizeof(yes));
+        if(ret != 0)
+        {
+            std::cerr << "Failed to configure the socket." << std::endl;
+            exit(EXIT_FAILURE);
+        }
         struct sockaddr_in serverAddr;
         memset(&serverAddr, 0, sizeof(serverAddr));
         serverAddr.sin_family = AF_INET;
-        serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
         serverAddr.sin_port = htons(port);
-
-        if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-            std::cerr << "Failed to bind the socket." << std::endl;
-            close(sockfd);
-            exit(EXIT_FAILURE);
+        if(serverName == NULL)
+            serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        else{
+            printf("getting host name %s\n", serverName);
+            server = gethostbyname(serverName);
+            if(server == NULL)
+            {
+                printf("failed to find host;\n");
+            }
+            serverAddr.sin_addr = *((struct in_addr *)server->h_addr);
         }
-
+        bzero(&(serverAddr.sin_zero), 8); 
+        if(serverName == NULL)
+            if (bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+                std::cerr << "Failed to bind the socket." << std::endl;
+                close(sockfd);
+                exit(EXIT_FAILURE);
+            }
+        printf("Socket id = %d",sockfd);
+        std::cout << "\tDiscovery Socket Created\n" << std::endl;
         return sockfd;
     }
 
-    void *discoveryThread(void *arg) {
-        int sockfd = createSocket(MONITORING_PORT);
+void *discoveryThread(void *arg) {
+        std::cout << "Starting Discovery\n" << std::endl;
+        int sockfd = createSocket(PORT,NULL);
         struct sockaddr_in clientAddr;
         char buffer[BUFFER_SIZE];
-
+        printf("before while\n");
+        socklen_t len = sizeof(clientAddr);
         while (1) {
+            std::cout << "while Discovery\n" << std::endl;
+            printf("in while\n");
             memset(buffer, 0, BUFFER_SIZE);
-            socklen_t len = sizeof(clientAddr);
-            int n = recvfrom(sockfd, (char*)buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr*)&clientAddr, &len);
+            printf("after memset\n");
+            std::cout << "while Discovery\n" << std::endl;
+            int n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&clientAddr, &len);
+            printf(" n= %d",n);
+            std::cout << "no n \n" << std::endl;
             if (n > 0) {
+                std::cout << "received packaged" << std::endl;
                 buffer[n] = '\0';
-
+                printf("message = %s",buffer);
+                std::cout << "\nmessage^\n" << std::endl;
                 // Extract the hostname from the buffer
                 char* hostname = strtok(buffer, ",");
                 if (hostname != NULL) {
                     // Extract the status from the buffer
+                    printf("hostname received = %s\n",hostname);
                     char* status = strtok(NULL, ",");
                     if (status != NULL) {
                         int i;
@@ -87,11 +121,48 @@ public:
                         }
                     }
                 }
+                
             }
         }
     }
-};
 
+int sendDiscoverypackaged()
+{
+    unsigned int length;
+    
+    char MyMac[] = "AA:BB:CC:DD:EE:FF";
+    char buffer[BUFFER_SIZE];
+    char serv_addr[] = "255.255.255.255"; // MY BROADCAST IP
+    int send,receive;
+    struct hostent *server;
+    int sockfd = createSocket(PORT_CLIENT,serv_addr);
+    length = sizeof(struct sockaddr_in);
+    struct sockaddr_in serveraddress;
+    serveraddress.sin_family = AF_INET;     
+	serveraddress.sin_port = htons(PORT);    
+    server = gethostbyname(serv_addr);
+    if(server == NULL)
+    {
+        printf("problem finding master server");
+    }
+	serveraddress.sin_addr = *((struct in_addr *)server->h_addr);
+    receive = 0; //Mudar para receber confirmacao
+    do{
+        printf("sending discovery packaged");
+        send = sendto(sockfd, MyMac, strlen(MyMac), 0, (const struct sockaddr *) &serveraddress, sizeof(struct sockaddr_in));
+        if (send < 0) 
+            printf("\nERROR sendto: %d \n",send);
+        else
+        {
+             //Mudar para receber confirmacao
+            //receive = recvfrom(sockfd, buffer, 256, 0, (struct sockaddr *) &serveraddress, &length);
+            //printf("received discovery packaged: %s",buffer);
+        }
+        
+    }while(send >=0 && receive >=0);
+    
+    return 0;
+}
 // Executado pelo manager
 // Monitora o status dos computadores a partir do envio de pacotes
 class monitoring_subservice{
@@ -244,14 +315,20 @@ int main(int argc, char *argv[]){
 
     if (argc > 1 && strcmp(argv[1], "manager") == 0) {
         isManager = true;
+        discovery_subservice discovery;
         std::cout << "Estação iniciada como Manager\n" << std::endl;
+        pthread_create(&discoveryThreadId, NULL, discoveryThread, NULL);
+        while(1)
+        {
+
+        }
     } else {
         std::cout << "Estação iniciada como Participante\n" << std::endl;
             std::string hostname = "MyComputer";
             std::string mac = "AA:BB:CC:DD:EE:FF";
             std::string ip = "192.168.1.100";
             bool is_awaken = true;
-
+            sendDiscoverypackaged();
             // Adicionar o participante na tabela
             management_subservice management;
             management.AddParticipantToTable(hostname, mac, ip, is_awaken);
